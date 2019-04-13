ddf2a7e34d024aa5192e6d7eea0740af49261873
mahsum97 <mahsum.yildirim97@gmail.com>
2019-04-14 01:52:58 +0200

    new123

--- app/src/main/java/com/example/finanzmanager/MainActivity.java
+++ app/src/main/java/com/example/finanzmanager/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        String test = "bye";
+        String test = "bye123";
 
         //Plus Button Weiterleitung
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.plus);
