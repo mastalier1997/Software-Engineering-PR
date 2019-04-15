9b22692203abe5925e2689698cdb96fef1137f50
FlorianPoessl <florian.poessl97@gmail.com>
2019-04-15 22:03:03 +0200

    Test

--- app/src/main/java/com/example/finanzmanager/MainActivity.java
+++ app/src/main/java/com/example/finanzmanager/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
+        //Test kommentar
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
