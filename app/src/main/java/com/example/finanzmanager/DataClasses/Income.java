67d160c9a3a29598afeb34223ff7b5ebda7f0b01
FlorianPoessl <florian.poessl97@gmail.com>
2019-06-23 10:27:12 +0200

    Issue fixes

--- app/src/main/java/com/example/finanzmanager/DataClasses/Income.java
+++ app/src/main/java/com/example/finanzmanager/DataClasses/Income.java
@@ -6,10 +6,14 @@ public class Income extends Position {
     private int id;
     static private int idCounter = 0;
 
-    Income(Date date, double value, boolean recurring, String category, String description) {
+    public Income(Date date, double value, boolean recurring, String category, String description) {
         super(date, value, recurring, category, description);
         this.id = idCounter;
         idCounter++;
+    }
+
+    public int getId() {
+        return id;
     }
 
 }
